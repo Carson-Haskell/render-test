4bd7d241486f19cc9b2eab1dd29e4596ae0a5c9f
Carson Haskell <carson.haskell@gmail.com>
2023-06-26 16:43:01 -0700

    update backend

--- backend/index.js
+++ backend/index.js
@@ -1,82 +1,85 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const Note = require("./models/note");
+
 app.use(express.json());
 app.use(express.static("build"));
 app.use(cors());
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => response.json(notes));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
-    response.json(note);
-  } else {
-    response.statusMessage = "No note found with matching id";
-    response.status(404).end();
-  }
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      note ? response.json(note) : response.status(404).end();
+    })
+    .catch(error => next(error));
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-  return maxId + 1;
+app.post("/api/notes", (request, response, next) => {
+  const { content, important } = request.body;
+
+  const note = new Note({
+    content,
+    important: important || false,
+  });
+
+  note
+    .save()
+    .then(savedNote => {
+      response.json(savedNote);
+    })
+    .catch(err => next(err));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then(updatedNote => response.json(updatedNote))
+    .catch(error => next(error));
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(_ => response.status(204).end())
+    .catch(error => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
+app.use(unknownEndpoint);
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  };
+  next(error);
+};
 
-  notes = [...notes, note];
+app.use(errorHandler);
 
-  response.json(note);
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-
-  response.status(204).end();
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
